cb0bc66c727f5b119354d4a1648935ea2237e164
HighBunkerHxH <JohnnyP95@hotmail.com>
2022-09-19 15:24:58 +0100

    Initial commit

---
+++ HelpSystem/HelpSystem/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A descrição da aplicação.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "A página de contatos.";
+
+            return View();
+        }
+
+        public ActionResult Questão()
+        {
+            ViewBag.Message = "As questões.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "Introduza as suas credenciais";
+
+            return View();
+        }
+    }
+}
