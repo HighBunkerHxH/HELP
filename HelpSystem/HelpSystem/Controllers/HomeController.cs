b9fe38816c5d75b1a61809d63b62ec3559d7775a
HighBunkerHxH <JohnnyP95@hotmail.com>
2022-09-23 10:48:33 +0100

    Login

--- HelpSystem/HelpSystem/Controllers/HomeController.cs
+++ HelpSystem/HelpSystem/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace HelpSystem.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "App description";
